db210256dfdb260866612c25950526ccd81ed9f5
Aladdin <g.tabroid@gmail.com>
2021-03-20 16:00:04 +0300

    sensetive keys store now it .env

---
+++ config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  prefix: '!',
+  token: process.env.token,
+  serverAddress: isProd ? process.env.prodServerAddress : process.env.devServerAddress,
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  publicKey: process.env.publicKey,
+};
