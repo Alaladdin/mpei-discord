19be4076138674f9b564dd6e4de9b488849a5933
Aladdin <g.tabroid@gmail.com>
2021-04-29 23:10:31 +0300

    v 0.6.3

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   isProd,
-  prefix: '!',
-  token: process.env.token,
+  prefix: process.env.PREFIX,
+  token: process.env.TOKEN,
   serverAddress: isProd ? process.env.PROD_SERVER : process.env.DEV_SERVER,
   channelIds: {
     actuality: isProd ? '832343311368192081' : '822176467978158100',
