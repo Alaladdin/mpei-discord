7bbbf13eaffd67f946a8d93b5ecd93fd34cae9aa
Aladdin <g.tabroid@gmail.com>
2021-05-01 17:32:13 +0300

    v 0.6.5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       name: 'mdis',
       script: 'index.js',
       watch: true,
-      ignore_watch: ['node_modules'],
+      ignore_watch: ['node_modules', 'store/localStore.json'],
       env: {
         NODE_ENV: 'development',
       },
