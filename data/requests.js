02f7326b8c656b088d987b41bb44df220ac46d20
Aladdin <g.tabroid@gmail.com>
2021-06-07 02:17:16 +0300

    v 0.8.3

--- data/requests.js
+++ data/requests.js
@@ -2,8 +2,8 @@ const { serverAddress, authToken } = require('../config');
 
 module.exports = {
   getUniversalUrl: (query) => `${serverAddress}/${query}`,
-  getConfigUrl: `${serverAddress}/getDiscordBotConfig?authToken=${authToken}`,
-  setConfigUrl: `${serverAddress}/setDiscordBotConfig?authToken=${authToken}`,
+  getConfigUrl: `${serverAddress}/getDiscordBotStore?authToken=${authToken}`,
+  setConfigUrl: `${serverAddress}/setDiscordBotStore?authToken=${authToken}`,
   getActualityUrl: `${serverAddress}/getActuality`,
   setActualityUrl: `${serverAddress}/setActuality?authToken=${authToken}`,
   getScheduleUrl: `${serverAddress}/getSchedule/`,
